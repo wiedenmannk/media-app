81f0b68980c1656e542aefdb6e4273944e976031
wiedenmannk <klaus.wiedenmann@yahoo.de>
2021-03-09 23:09:58 +0100

    add primeng and bootstrap angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AccordionModule } from "primeng/accordion";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgbModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
